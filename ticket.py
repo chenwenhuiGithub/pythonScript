d6b191235c4b3d332bfedd6cfb8002907fe028ea
mingyan.chen <mingyan.chen@tuya.com>
2022-08-26 16:46:23 +0800

    ticket: login success

--- ticket.py
+++ ticket.py
@@ -2,7 +2,10 @@
 import requests
 import json
 import re
+import time
 import qrcode
+import os
+import base64
 import prettytable as pt
 
 
@@ -20,6 +23,12 @@ class ticket:
         self.login_url = ''
         self.jsessionid = ''
         self.uuid = ''
+        # TODO: get rail_deviceid and rail_expiration
+        self.rail_deviceid = 'ri4SGw5nqap-u7g3dyz6X3EtPJAdqaGeNm9-n94W-stEgktjUeHm1kcMPm6HIJMptC4sK7jZke5RIgC6U0cBWJtdx8EhwAnSzaB55RR65TwV_HeoGo5H_oLPMukVmPLswj-wxh73iEIGEYokiYpI7ISQHzgv6EIK'
+        self.rail_expiration = '1661601931084'
+        self.qrcode_filename = 'qrcode.png'
+        self.newapptk = ''
+        self.username = ''
 
     def get_station_code(self):
         url = 'https://www.12306.cn/index/script/core/common/station_name_v10178.js'
@@ -92,22 +101,111 @@ class ticket:
 
     def gen_qrcode(self):
         url = 'https://kyfw.12306.cn/passport/web/create-qr64'
-        params = {
+        data = {
             'appid':'otn'
         }
-        resp = self.session.post(url, params=params, headers=self.headers)
+        resp = self.session.post(url, data=data, headers=self.headers)
         resp.encoding = 'utf-8'
         dic = json.loads(resp.text)
 
         self.uuid = dic['uuid']
         print('uuid:', self.uuid)
 
-        qr = qrcode.QRCode(error_correction=qrcode.ERROR_CORRECT_L, box_size=5, border=2)
-        qr.add_data(dic['image'])
+        image_bytes = base64.b64decode(dic['image'])
+        with open(self.qrcode_filename, 'wb') as f:
+            f.write(image_bytes)
+        os.startfile(self.qrcode_filename)
+
+        #qr = qrcode.QRCode(error_correction=qrcode.ERROR_CORRECT_L, box_size=5)
+        #qr.add_data(image_bytes)
         # qr.print_ascii(invert=False)
-        qr.make_image().show() # pip3 install image
+        #qr.make_image().show() # pip3 install image
         print('get qrcode success, please scan')
 
+    def check_qrcode(self):
+        url = 'https://kyfw.12306.cn/passport/web/checkqr'
+        while True:
+            params = {
+                'RAIL_DEVICEID':self.rail_deviceid,
+                'RAIL_EXPIRATION':self.rail_expiration,
+                'uuid':self.uuid,
+                'appid':'otn'
+            }
+            resp = self.session.post(url, params=params, headers=self.headers)
+            resp.encoding = 'utf-8'
+            dic = json.loads(resp.text)
+
+            result_code = dic['result_code']
+            if result_code == '0': # not scan
+                print('qrcode not scan')
+            elif result_code == 1: # scan success
+                print('qrcode scan success, please confirm on phone')
+            elif result_code == 2: # login success
+                print('qrcode login success')
+                return
+            elif result_code == 3: # qrcode invalid
+                print('qrcode scan timeout')
+                return
+            else:
+                print('qrcode login failed, result code:%s', result_code)
+                return
+            time.sleep(1)
+
+    def auth_uamtk(self):
+        url = 'https://kyfw.12306.cn/passport/web/auth/uamtk'
+        data = {
+            'appid':'otn'
+        }
+        resp = self.session.post(url, data=data, headers=self.headers)
+        resp.encoding = 'utf-8'
+        dic = json.loads(resp.text)
+
+        self.newapptk = dic['newapptk']
+        print('newapptk:', self.newapptk)
+
+    def auth_uamauthclient(self):
+        url = 'https://kyfw.12306.cn/otn/uamauthclient'
+        data = {
+            'tk':self.newapptk
+        }
+        resp = self.session.post(url, data=data, headers=self.headers)
+        resp.encoding = 'utf-8'
+        dic = json.loads(resp.text)
+
+        self.username = dic['username']
+        print('username:', self.username)
+
+    def initMy12306Api(self):
+        url = 'https://kyfw.12306.cn/otn/index/initMy12306Api'
+        resp = self.session.post(url, headers=self.headers)
+        resp.encoding = 'utf-8'
+        dic = json.loads(resp.text)
+        print('login success ' + self.username + ' ' + dic['user_regard'])
+
+    def query_passengers(self):
+        url = 'https://kyfw.12306.cn/otn/passengers/query'
+        data = {
+            'pageIndex':'1',
+            'pageSize':'10'
+        }
+        resp = self.session.post(url, data=data, headers=self.headers)
+        resp.encoding = 'utf-8'
+        dic = json.loads(resp.text)
+
+        tb = pt.PrettyTable()
+        tb.field_names = ['姓名', '性别', '生日', '证件号码', '手机/电话', '旅客类型']
+
+        passenger_list = dic['data']['datas']
+        for passenger_item in passenger_list:
+            tb.add_row([
+                passenger_item['passenger_name'],
+                passenger_item['sex_name'],
+                passenger_item['born_date'],
+                passenger_item['passenger_id_no'],
+                passenger_item['mobile_no'],
+                passenger_item['passenger_type_name']
+            ])
+        print(tb)
 
 if __name__ == '__main__':
     ticket_object = ticket('杭州', '信阳', '2022-09-01')
@@ -115,3 +213,8 @@ if __name__ == '__main__':
     ticket_object.get_index_login_conf()
     ticket_object.query_left_ticket()
     ticket_object.gen_qrcode()
+    ticket_object.check_qrcode()
+    ticket_object.auth_uamtk()
+    ticket_object.auth_uamauthclient()
+    ticket_object.initMy12306Api()
+    ticket_object.query_passengers()
